6009cf335b4235bfe4900e97ff2b4855c9fb3689
kumartul <kumartul001@gmail.com>
2022-04-17 12:29:03 +0530

    [Create]: Error classes

---
+++ src/errors/maxLengthExceededError.js
@@ -0,0 +1,10 @@
+class MaxLengthExceddedError extends Error {
+    constructor(message) {
+        super(message);
+
+        this.name = 'MaxLengthExceddedError';
+        this.message = message;
+    }
+}
+
+export default MaxLengthExceddedError;
---
+++ src/errors/negativeLengthError.js
@@ -0,0 +1,10 @@
+class NegativeLengthError extends RangeError {
+    constructor(message) {
+        super(message);
+        
+        this.name = 'NegativeLengthError';
+        this.message = message;
+    }
+}
+
+export default NegativeLengthError;
