c47128ccc453ba85ef72c11322587142d37f3529
kumartul <kumartul001@gmail.com>
2022-04-20 19:31:55 +0530

    [Update]: Imports

--- example/src/index.js
+++ example/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-import './style/index.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
