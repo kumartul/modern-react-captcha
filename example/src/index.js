162a257f3109da4e2469a2e41e35b72d5a921c27
kumartul <kumartul001@gmail.com>
2022-04-20 19:20:18 +0530

    [Refactor]: Fix indentation

--- example/src/index.js
+++ example/src/index.js
@@ -7,7 +7,7 @@ import './style/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
