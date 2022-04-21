2dd40e47cc1f6ef8fbfecc733fa10c8994475850
kumartul <kumartul001@gmail.com>
2022-04-21 16:45:07 +0530

    [Fix]: Form submit on reload button click bug

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ const ReactCaptcha = ({ charset, length, color, bgColor, reload, reloadText, rel
         <div className="modern-react-captcha">
             <RandomText text={captcha} color={color} bgColor={bgColor} />
 
-            {reload && <button className='modern-react-captcha__reloadBtn' onClick={reloadCaptcha}>{reloadText}{reloadIcon && <img src={reloadIcon} alt='Reload' style={{ width: '20px', height: '20px' }} />}</button>}
+            {reload && <button type="button" className='modern-react-captcha__reloadBtn' onClick={reloadCaptcha}>{reloadText}{reloadIcon && <img src={reloadIcon} alt='Reload' style={{ width: '20px', height: '20px' }} />}</button>}
 
             <input ref={inputRef} type="text" placeholder="Enter captcha" className='modern-react-captcha__inputField' />
             <button type="button" onClick={evaluateCaptcha} className='modern-react-captcha__submitBtn'>Submit</button>
